ce96595f9c5ea25c32ad6b6ab5bf3005d22d8e16
Kevin Windisch <kwindisch1992@gmail.com>
2016-01-10 17:34:48 -0600

    Not currently working but I think I'm onto something

--- lib/app.rb
+++ lib/app.rb
@@ -73,6 +73,61 @@ def print_heading heading
   end 
 end
 
+def calc_product_data toy
+
+  # Calculate the total amount of sales and average price sold for
+  sales_sum = 0
+  sales_avg = 0
+  num_of_purchases = toy["purchases"].length
+  toy["purchases"].each do |purchases|
+      
+      sales_sum += purchases["price"]
+      sales_avg = sales_sum / num_of_purchases
+    end
+
+  # Calculate the average discount based off the average sales price
+  full_price = toy["full-price"].to_i
+  avg_discount = ((full_price - sales_avg)/full_price) * 100
+
+end
+
+
+def print_product_data toy
+
+    # Print the name of the toy
+    puts "Toy Name:  #{toy["title"]}"
+
+    # Print the retail price of the toy
+    puts "Toy Price: #{toy["full-price"]}"
+
+    # Print the total number of purchases
+    puts "Total Number of Purchases: #{num_of_purchases}"
+
+    # Print the total amount of sales
+    puts "Total Amount Sold: $#{sales_sum}"
+
+    # Print average price the toy sold for
+    puts "Average Selling Price: $#{sales_avg}"
+
+    # Print average discount based off average sales price
+    puts "Average Discount: #{avg_discount.round(2)}%"
+
+end
+
+def compile_products_section toy
+
+  calc_product_data(toy)
+  print_product_data(toy)
+    
+end
+
+def make_products_section
+
+  $products_hash["items"].each do |toy|
+    compile_products_section(toy)
+
+  end
+end
 
 
 def create_report
